9ef7edf7c698e5c53655faa6d18e27409eb883f5
Muhammed Riza <rizach234@gmail.com>
2025-02-10 11:25:04 +0530

    Add files via upload

---
+++ imag_classf/model.py
@@ -0,0 +1,62 @@
+# Importing the libraries
+import pandas as pd
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+import os
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+from skimage.io import imread
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,accuracy_score,confusion_matrix
+import pickle
+from IPython.display import Image, display
+from io import BytesIO
+import requests
+from sklearn.metrics import accuracy_score
+
+Categories=['Aeroplanes','Cars']
+flat_data_arr=[]
+target_arr=[]
+#please use datadir='/content' if the files are upload on to google collab
+#else mount the drive and give path of the parent-folder containing all category images folders.
+datadir=r'C:\Users\Muhammed Riza\OneDrive\Documents\MSC AI,ML AND DS\sem3\ibm sem2 project\categories'
+for i in Categories:
+  print(f'loading... category : {i}')
+  path=os.path.join(datadir,i)
+  for img in os.listdir(path):
+    img_array=imread(os.path.join(path,img))
+    img_resized=resize(img_array,(150,150,3))
+    flat_data_arr.append(img_resized.flatten())
+    target_arr.append(Categories.index(i))
+  print(f'loaded category:{i} successfully')
+flat_data=np.array(flat_data_arr)
+target=np.array(target_arr)
+df=pd.DataFrame(flat_data)
+df['Target']=target
+df
+
+x=df.iloc[:,:-1]
+y=df.iloc[:,-1]
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=77,stratify=y)
+print('Splitted Successfully')
+
+
+
+param_grid={'C':[0.1,1,10,100],'gamma':[0.0001,0.001,0.1,1],'kernel':['rbf','poly']}
+svc=svm.SVC(probability=True)
+print("The training of the model is started, please wait for while as it may take few minutes to complete")
+model=GridSearchCV(svc,param_grid)
+model.fit(x_train,y_train)
+print('The Model is trained well with the given images')
+model.best_params_
+
+y_pred=model.predict(x_test)
+
+
+#classification_report(y_pred,y_test)
+print(f"The model is {accuracy_score(y_pred,y_test)*100}% accurate")
+#confusion_matrix(y_pred,y_test)
+
+pickle.dump(model,open('img_model.p','wb'))
+print("Pickle is dumped successfully")
