13f930ea8cbb143b5b117c86da315673ad07ff50
Muhammed Riza <rizach234@gmail.com>
2025-02-12 17:18:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def home():
     return render_template("index.html", prediction_result=prediction_result, image_url=image_url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
